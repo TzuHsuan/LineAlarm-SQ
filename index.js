7c0532a0f2e6949d8bfbcb019756392a89cead35
Andy Lin <AndyHsuanLin@gmail.com>
2018-09-20 23:18:13 +1000

    add maple

--- index.js
+++ index.js
@@ -23,9 +23,130 @@ const baseURL = process.env.BASE_URL;
 
 const sqclient = new line.Client(sqconfig);
 
-//const mapleclient = new line.Client(mapleconfig);
+const mapleclient = new line.Client(mapleconfig);
 
 const app = express();
+
+//maple
+new cron('0 0 7 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'小夥伴們早安，大家吃早餐了嗎？'+eol+'記得要去『公會地城』刷一下喔！'+eol+'小秘書關心你～');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 10 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'嗨嗨，現在10點囉，各位小夥伴要記得上『公會地城』喔。'+eol+'小夥伴們要定時去看一下版規喔，我們會不定時更新。'+eol+'小秘書提醒你');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 11 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'嗨嗨，小秘書我又出現了owo'+eol+'倒數一小時要打王團囉，請小夥伴們準備'+eol+'記得先看『王團筆記本的規定』哦，不要當白白哦！');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 12 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'打王團啦!! 記得看一下『王團筆記本的規則』喔!!'+eol+'趕快報名 +1+1 !!'+eol+'小秘書替你們加加油 !!');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 13 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'注意(๑•̀ω•́๑)'+eol+'公會地城記得打'+eol+'公會地城記得打'+eol+'今日起簽到部分由公會地城計算，所以不要遺忘「公會地城」嘍'+eol+'記得不要忘記，加油');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 19 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'嗨嗨，小夥伴們都吃飽飽了吧！'+eol+'倒數一小時要打王團囉，請小夥伴們準備囉'+eol+'記得先看『王團筆記本的規定』哦，不要當白白哦。');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 20 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'打王團啦!! 記得看一下『王團筆記本的規則』喔!!'+eol+'趕快報名 +1+1 !!'+eol+'小秘書替你們加加油 !!');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 21 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'注意(๑•̀ω•́๑)'+eol+'公會地城記得打'+eol+'公會地城記得打'+eol+'今日起簽到部分由公會地城計算，所以不要遺忘「公會地城」嘍'+eol+'記得不要忘記，加油'+eol+'(*๓´╰╯`๓)'+eol+'「4會公宴開始～～」');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 30 21 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'倒數30分鐘後公會宴會'+eol+'請小夥伴們準時入座'+eol+'才不會人擠人擠不進去喔！');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
+
+new cron('0 0 22 * * *',() =>{
+	db.query('SELECT * FROM maple')
+	.then(result => {
+		if(result.rows.length>0){
+			result.rows.map(r=>{
+				pushMessage(mapleclient, r.id,'公宴啦 !!! 開飯飯囉！'+eol+'大家都進去了嗎？'+eol+'要幫小秘書打『公會地城』喔，幫助公會排名。');
+			})
+		}
+	})
+	.catch(err=>console.error(err.stack));
+},null,true,'Asia/Taipei');
 
 
 //公單
@@ -109,10 +230,23 @@ app.post('/callback/sq', line.middleware(sqconfig), (req, res) => {
 	});
 });
 
+app.post('/callback/maple', line.middleware(mapleconfig), (req, res) => {
+
+	if(!Array.isArray(req.body.events)){
+		return res.status(500).end()
+	}
+
+	Promise.all(req.body.events.map(maplehandleEvent))
+	.then(()=>res.end())
+	.catch((err)=>{
+		console.log(err);
+		res.status(500).end();
+	});
+});
+
 
 const replyText = (client, token, message) => {
 	message = Array.isArray(message)? message:[message];
-	console.log(client);
 	return client.replyMessage(
 		token,
 		message.map((message)=>({type:'text', text:message}))
@@ -186,7 +320,7 @@ function sqhandleEvent(event){
 		case 'message':
 			switch (event.message.type){
 				case 'text':
-					return handleText(event.message, event.replyToken, event.source);
+					return sqhandleText(event.message, event.replyToken, event.source);
 				case 'image':
 				case 'video':
 				case 'audio':
@@ -223,7 +357,50 @@ function sqhandleEvent(event){
 	}
 }
 
-function handleText(message, replyToken, source){
+function maplehandleEvent(event){
+	switch (event.type){
+		case 'message':
+			switch (event.message.type){
+				case 'text':
+					//return handleText(event.message, event.replyToken, event.source);
+				case 'image':
+				case 'video':
+				case 'audio':
+				case 'location':
+				case 'sticker':
+					//ignore event
+					return 0;
+				default:
+					throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+			}
+		case 'follow':
+		case 'join':
+			sub('maple', value);
+			return replyText(mapleclient, event.replyToken, '大家好，這裡是楓谷小秘書～');
+
+		case 'unfollow':
+		case 'leave':
+			var value;
+			switch (event.source.type){
+				case 'user':
+					value = event.source.userId;
+					break;
+				case 'group':
+					value = event.source.groupId;
+					break;
+				case 'room':
+					value = event.source.roomId;
+					break;
+			}
+			unsub('maple', value);
+			break;
+
+		default:
+			throw new Error(`Unknown even: ${JSON.stringify(event)}`);
+	}
+}
+
+function sqhandleText(message, replyToken, source){
 	switch(message.text){
 		case '!訂閱 全部':
 			sqsubAll(source);
