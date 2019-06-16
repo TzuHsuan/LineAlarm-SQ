5394002a1b413620ea5b61e73fbf947809f95281
Andy Lin <AndyHsuanLin@gmail.com>
2019-06-16 12:12:34 +1000

    refactor

---
+++ dbUtil.js
@@ -0,0 +1,50 @@
+const {Pool} = require('pg');
+
+const db = new Pool({
+	connectionString: process.env.DATABASE_URL,
+  	ssl: true
+})
+
+const tableList = ['public_order', 'bento', 'royal', 'goodnight']
+
+const dbUtil = {
+	loadSub = () => {
+		let subscribers = {};
+		tableList.forEach( item => {
+			db.query(`SELECT * FROM ${item}`)
+			.then(result => {
+				subscribers[item] = result.rows;
+			})
+			.catch(err=>console.error(err.stack));
+		})
+		return subscribers;	
+	},
+	sub = (target, source) => {
+		let value = source.userId||source.groupId||source.roomId;
+		target === 'all' ? (target = tableList) : (target = [target]);
+		target.forEach(table => {
+			db.query(`INSERT INTO ${table} VALUES($1) RETURNING *`, [value])
+				.then(res => {
+					console.log(res.rows[0]);
+				})
+				.catch(e => {
+					console.error(e.stack)
+			});
+		})	
+	},
+	unSub = (target, source) => {
+		let value = source.userId||source.groupId||source.roomId;
+		target === 'all' ? (target = tableList) : (target = [target]);
+		target.forEach(table => {
+			db.query(`DELETE FROM ${table} WHERE id = $1`, [value])
+				.then(res => {
+					console.log(res.rows[0]);
+				})
+				.catch(e => {
+					console.error(e.stack)
+			});
+		})	
+	}
+}
+
+export default dbUtil;
