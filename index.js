347c2fd57413949a87cffc73099b9be73842f32c
Andy Lin <AndyHsuanLin@gmail.com>
2019-06-16 14:57:29 +1000

    bug fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const eol = require('os').EOL;
 const cron = require('cron').CronJob;
 const dbUtil = require('./dbUtil.js');
-const messages = require('./messages.json');
+const messages = require('./messages.json').messages;
 
 
 const sqconfig = {
@@ -81,50 +81,7 @@ switch (event.type){
 		case 'unfollow':
 		case 'leave':
 			var value = event.source.userId||event.source.groupId||event.source.roomId;
-			dbUtil.sub('all', value);
-			break;
-
-		default:
-			throw new Error(`Unknown even: ${JSON.stringify(event)}`);
-	}
-}
-
-function maplehandleEvent(event){
-	switch (event.type){
-		case 'message':
-			switch (event.message.type){
-				case 'text':
-					//return handleText(event.message, event.replyToken, event.source);
-				case 'image':
-				case 'video':
-				case 'audio':
-				case 'location':
-				case 'sticker':
-					//ignore event
-					return 0;
-				default:
-					throw new Error(`Unknown message: ${JSON.stringify(message)}`);
-			}
-		case 'follow':
-		case 'join':
-			sub('maple', event.source);
-			return replyText(mapleclient, event.replyToken, '大家好，這裡是楓谷小秘書～');
-
-		case 'unfollow':
-		case 'leave':
-			var value;
-			switch (event.source.type){
-				case 'user':
-					value = event.source.userId;
-					break;
-				case 'group':
-					value = event.source.groupId;
-					break;
-				case 'room':
-					value = event.source.roomId;
-					break;
-			}
-			unsub('maple', value);
+			db.sub('all', value);
 			break;
 
 		default:
@@ -135,47 +92,47 @@ function maplehandleEvent(event){
 function sqhandleText(message, replyToken, source){
 	switch(message.text){
 		case '!訂閱 全部':
-		dbUtil.sub('all', source);
+			db.sub('all', source);
 			return replyText(replyToken, '已訂閱所有通知');
 		case '!訂閱 公單':
-			dbUtil.sub('public_order', source);
+			db.sub('public_order', source);
 			return replyText(replyToken, '已訂閱公單提醒');
 		case '!訂閱 晚安':
-			dbUtil.sub('goodnight', source);		
+			db.sub('goodnight', source);		
 			return replyText(replyToken, '已訂閱晚安訊息');
 		case '!訂閱 便當':
-			dbUtil.sub('bento', source);		
+			db.sub('bento', source);		
 			return replyText(replyToken, '已訂閱便當提醒');
 		case '!訂閱 皇家':
-			dbUtil.sub('royal', source);		
+			db.sub('royal', source);		
 			return replyText(replyToken, '已訂閱皇家提醒');
 		case '!訂閱 天城':
-			dbUtil.sub('arena', source);		
+			db.sub('arena', source);		
 			return replyText(replyToken, '已訂閱天城提醒');
 		case '!訂閱 世界王':
-			dbUtil.sub('boss', source);		
+			db.sub('boss', source);		
 			return replyText(replyToken, '已訂閱世界王提醒');
 
 		case '!取消 公單':
-			dbUtil.unsub('public_order', source)
+			db.unsub('public_order', source)
 			return replyText(replyToken, '已取消公單提醒');
 		case '!取消 搶劫':
-			dbUtil.unsub('rob', source);	
+			db.unsub('rob', source);	
 			return replyText(replyToken, '已取消搶劫提醒');
 		case '!取消 晚安':
-			dbUtil.unsub('goodnight', source);		
+			db.unsub('goodnight', source);		
 			return replyText(replyToken, '已取消晚安訊息');
 		case '!取消 便當':
-			dbUtil.unsub('bento', source);		
+			db.unsub('bento', source);		
 			return replyText(replyToken, '已取消便當提醒');
 		case '!取消 皇家':
-			dbUtil.unsub('royal', source);		
+			db.unsub('royal', source);		
 			return replyText(replyToken, '已取消皇家提醒');
 		case '!取消 天城':
-			dbUtil.unsub('bento', source);		
+			db.unsub('bento', source);		
 			return replyText(replyToken, '已取消天城提醒');
 		case '!取消 世界王':
-			dbUtil.unsub('royal', source);		
+			db.unsub('royal', source);		
 			return replyText(replyToken, '已取消世界王提醒');
 
 		case '!離開':
@@ -195,7 +152,7 @@ function sqhandleText(message, replyToken, source){
 			}
 			//fallthrough to unsub all
 		case '!取消 全部'	:
-		dbUtil.unsub('all',source);
+		db.unsub('all',source);
 			return replyText(replyToken, '已取消所有通知');
 		default:
 			return 0;
